734640b33456cd0e3251c3037d91c7f5e502147e
Công Tiến Trần <tctien342@gmail.com>
2025-02-15 14:35:50 +0700

    fix(types): remove unused types

--- types/component.d.ts
+++ types/component.d.ts
@@ -1,16 +1,4 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
-type TAny = ReturnType<JSON.values>;
-
 interface IComponent<T = {}> extends React.FC<React.PropsWithChildren<T>> {}
 interface ISvgComponent<T = {}>
   extends IComponent<React.SVGProps<SVGSVGElement> & T> {}
-interface IFilterControllerComponent<T = {}>
-  extends IComponent<TFilterSectionValue & T> {}
-
-// type TNextPage = import('next').NextPage<{ params: { lang: import('../i18n-config').Locale } }>;
-
-type TNextPage<T = unknown> = import("next").NextPage<{
-  params: { lang: import("../i18n-config").Locale } & T;
-}>;
-
-type ArrayElementType<ArrayType extends Array> = ArrayType[number];
